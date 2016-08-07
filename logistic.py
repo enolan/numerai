efd1e331ad71671d0a898302ad0cb9359ce56edc
Echo Nolan <echo@echonolan.net>
2016-08-06 20:41:28 -0700

    Use Tensorboard + RMSProp

--- logistic.py
+++ logistic.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 from train import *
 
 weights = tf.Variable(tf.random_uniform([21, 1], dtype=tf.float32))
+tf.histogram_summary("weights", weights)
 
 def predict(xs):
     return 1 / (1 + tf.exp(-tf.matmul(xs, weights)))
--- train.py
+++ train.py
@@ -12,20 +12,31 @@ def train(predictor):
 
     preds = predictor(xs=xs)
 
-    opt = tf.train.GradientDescentOptimizer(0.1)
+    opt = tf.train.RMSPropOptimizer(0.03)
     opt_op = opt.minimize(logLoss(preds, ys))
+
+    loss = logLoss(preds, ys)
+
+    tf.scalar_summary("loss", loss)
+    merged = tf.merge_all_summaries()
+    trainWriter = tf.train.SummaryWriter("logs/train", sess.graph)
+    testWriter = tf.train.SummaryWriter("logs/test")
 
     tf.initialize_all_variables().run()
 
-    for i in range(1000000):
+    for i in range(trainData.shape[0]*5):
         batchFeatures, batchYs = getMinibatch()
         opt_op.run(feed_dict={xs: batchFeatures, ys: batchYs})
         if i % 100 == 0:
-            trainLoss = logLoss(preds, ys).eval(
-                feed_dict={ys: batchYs, xs: batchFeatures})
+            trainLoss, trainLossSummary = sess.run(
+                [loss, merged],
+                feed_dict = {ys: batchYs, xs: batchFeatures})
+            trainWriter.add_summary(trainLossSummary, i)
             testFeatures, testYs = getTestFeatures(), getTestYs()
-            testLoss = logLoss(preds, ys).eval(
-                feed_dict={ys: testYs, xs: testFeatures})
+            testLoss, testLossSummary = sess.run(
+                [loss, merged],
+                feed_dict = {ys: testYs, xs: testFeatures})
+            testWriter.add_summary(testLossSummary, i)
             print(
                 'Batch {:6}, epoch {:f}, train loss {:f}, test loss {:f}'
                 .format(i, i/trainData.shape[0], trainLoss, testLoss))
