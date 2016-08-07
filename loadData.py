95509a804246cc5ce89ddd0f3f899844ac86c68a
Echo Nolan <echo@echonolan.net>
2016-08-06 18:16:18 -0700

    Separate test and train data

--- loadData.py
+++ loadData.py
@@ -1,13 +1,32 @@
 import numpy
+import math
+
+numpy.random.seed(19900515)
 
 dataDir = "data-2016-08-03"
-
 dataDir = "data/" + dataDir
 
-train = numpy.loadtxt(dataDir + "/numerai_training_data.csv", skiprows=1, delimiter=',')
+trainDataIn = numpy.loadtxt(dataDir + "/numerai_training_data.csv", skiprows=1, delimiter=',')
+numpy.random.shuffle(trainDataIn)
+
+splits = numpy.split(trainDataIn, [math.floor(trainDataIn.shape[0]*0.9)])
+trainData = splits[0]
+testData = splits[1]
+
+def getFeatures(mat):
+    return mat[:, :-1]
+
+def getYs(mat):
+    return mat[:, -1:]
 
 def getTrainFeatures():
-    return train[:, 0:20]
+    return getFeatures(trainData)
 
 def getTrainYs():
-    return train[:, 21]
+    return getYs(trainData)
+
+def getTestFeatures():
+    return getFeatures(testData)
+
+def getTestYs():
+    return getYs(testData)
--- logistic.py
+++ logistic.py
@@ -7,11 +7,10 @@ sess = tf.InteractiveSession()
 ysDat = getTrainYs()
 xsDat = getTrainFeatures()
 
-inYs = tf.placeholder(tf.float32)
-ys = tf.reshape(inYs, [ysDat.shape[0], 1])
+ys = tf.placeholder(tf.float32, shape=[None, 1])
 
-xs = tf.placeholder(tf.float32)
-weights = tf.Variable(tf.random_uniform([20, 1], dtype=tf.float32))
+xs = tf.placeholder(tf.float32, shape=[None, 21])
+weights = tf.Variable(tf.random_uniform([21, 1], dtype=tf.float32))
 preds = 1 / (1 + tf.exp(-tf.matmul(xs, weights)))
 
 opt = tf.train.GradientDescentOptimizer(0.1)
@@ -20,5 +19,5 @@ opt_op = opt.minimize(logLoss(preds, ys))
 tf.initialize_all_variables().run()
 
 for x in range(1000):
-    opt_op.run(feed_dict={inYs: ysDat, xs: xsDat})
-    print(logLoss(preds, ys).eval(feed_dict={inYs: ysDat, xs: xsDat}))
+    opt_op.run(feed_dict={ys: ysDat, xs: xsDat})
+    print(logLoss(preds, ys).eval(feed_dict={ys: ysDat, xs: xsDat}))
