f481171a1ab54fbb81e86f9e1b5ede223ba7f440
Echo Nolan <echo@echonolan.net>
2016-08-13 14:35:20 -0700

    Make Tensorboard better in small-nn.py

--- small-nn.py
+++ small-nn.py
@@ -5,22 +5,23 @@ def computeKeep(isTraining, prob):
     return (1 - isTraining * (1 - prob))
 
 def mkLayer(xs, outputs, keep_prob, act_fn, maxNormConstraint, name):
-    numInputs = xs.get_shape().as_list()[-1]
-    weight_shape = [numInputs, outputs]
-    weights = tf.Variable(tf.truncated_normal(weight_shape, stddev=tf.sqrt(2. / tf.to_float(numInputs)), dtype=tf.float32, name=name + "-weights"))
-    tf.histogram_summary(name + "-weights", weights)
-    norms = tf.sqrt(tf.reduce_sum(weights ** 2, reduction_indices=1))
-    tf.histogram_summary(name + "-norm-weighs", norms)
-    maxNorm = tf.reduce_max(norms)
-    tf.scalar_summary(name + "-max-norm", tf.reduce_max(norms))
-    biases = tf.Variable(tf.constant(0.01, shape = [outputs]))
-    tf.histogram_summary(name + "biases", biases)
-    sums = tf.matmul(xs, weights) + biases
-    sumsDropped = tf.nn.dropout(sums, keep_prob)
-    activations = act_fn(sumsDropped)
-    tf.histogram_summary(name + "-activations", activations)
-    maxNormOp = weights.assign(tf.clip_by_norm(weights, maxNormConstraint, axes=[1]))
-    return activations, maxNormOp # tf.Print(activations, [maxNorm])
+    with tf.name_scope("mkLayer-" + name) as scope:
+        numInputs = xs.get_shape().as_list()[-1]
+        weight_shape = [numInputs, outputs]
+        weights = tf.Variable(tf.truncated_normal(weight_shape, stddev=tf.sqrt(2. / tf.to_float(numInputs)), dtype=tf.float32, name=name + "-weights"))
+        tf.histogram_summary(name + "-weights", weights)
+        norms = tf.sqrt(tf.reduce_sum(weights ** 2, reduction_indices=1))
+        tf.histogram_summary(name + "-norm-weights", norms)
+        maxNorm = tf.reduce_max(norms)
+        tf.scalar_summary(name + "-max-norm", tf.reduce_max(norms))
+        biases = tf.Variable(tf.constant(0.01, shape = [outputs]))
+        tf.histogram_summary(name + "-biases", biases)
+        sums = tf.matmul(xs, weights) + biases
+        sumsDropped = tf.nn.dropout(sums, keep_prob)
+        activations = act_fn(sumsDropped)
+        tf.histogram_summary(name + "-activations", activations)
+        maxNormOp = weights.assign(tf.clip_by_norm(weights, maxNormConstraint, axes=[1]))
+        return activations, maxNormOp # tf.Print(activations, [maxNorm])
     # return tf.Print(tf.nn.relu(sumsDropped), [sumsDropped], summarize=1000000000)
 
 def predict(xs, isTraining):
