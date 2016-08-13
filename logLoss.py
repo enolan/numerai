4141073ceb8705ada847f095d28a6277201b34fe
Echo Nolan <echo@echonolan.net>
2016-08-13 02:49:50 -0700

    Group log loss in TensorBoard graph

--- logLoss.py
+++ logLoss.py
@@ -1,9 +1,10 @@
 import tensorflow as tf
 
 def logLoss(preds, ys):
-    epsilon = 1e-7
-    preds2 = tf.maximum(preds, epsilon)
-    preds2 = tf.minimum(preds2, 1-epsilon)
-    posLogLoss = tf.mul(ys, tf.log(preds2))
-    negLogLoss = tf.mul(1 - ys, tf.log(1 - preds2))
-    return -tf.reduce_mean(tf.add(posLogLoss, negLogLoss))
+    with tf.name_scope("logLoss") as scope:
+        epsilon = 1e-7
+        preds2 = tf.maximum(preds, epsilon)
+        preds2 = tf.minimum(preds2, 1-epsilon)
+        posLogLoss = tf.mul(ys, tf.log(preds2))
+        negLogLoss = tf.mul(1 - ys, tf.log(1 - preds2))
+        return -tf.reduce_mean(tf.add(posLogLoss, negLogLoss))
