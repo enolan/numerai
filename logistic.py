2e3c4547a4693a3f80a67dc1c8f3a8a6466150b3
Echo Nolan <echo@echonolan.net>
2016-08-13 12:43:20 -0700

    fix logistic regression

--- logistic.py
+++ logistic.py
@@ -1,10 +1,10 @@
 import tensorflow as tf
 from train import *
 
-weights = tf.Variable(tf.random_uniform([21, 1], dtype=tf.float32))
+weights = tf.Variable(tf.random_uniform([21, 1], minval=-0.1, maxval=0.1, dtype=tf.float32))
 tf.histogram_summary("weights", weights)
 
 def predict(xs, _isTraining):
-    return 1 / (1 + tf.exp(-tf.matmul(xs, weights)))
+    return 1 / (1 + tf.exp(-tf.matmul(xs, weights))), tf.no_op()
 
 go(predict, "logistic")
